64bef78f0022017b9595aaec00638c82be3d7347
masic1 <masic.mirza@gmail.com>
2021-09-29 23:26:38 +0200

    Still working

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -2,7 +2,8 @@ let foo = 12, boo = 45;
 
 function main(foo, boo) {
     // Only change code below this one
-    const [foo, boo] = [45, 12];
+
+    [foo, boo] = [boo, foo];
     // Only change code above this one
     return {foo, boo};
 }
